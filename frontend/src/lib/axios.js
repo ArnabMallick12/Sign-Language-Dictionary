09f736181b8c513b042e645aa55cf2f22a35053b
Arnab <arnavmallick12006@gmail.com>
2025-05-26 15:11:55 +0530

    somes fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';  
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://sign-language-dictionary.onrender.com/api',
   withCredentials: true, // Enable sending cookies with requests
   headers: {
     'Content-Type': 'application/json',
